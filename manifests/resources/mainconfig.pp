af2465364a201ff63228abdc4cc0ef9ad186265d
s1rc0 <sergey.postument@gmail.com>
2014-12-11 17:52:02 +0200

    test if statement v3

--- manifests/resources/mainconfig.pp
+++ manifests/resources/mainconfig.pp
@@ -1,26 +1,26 @@
 define nginx::resources::mainconfig(
-  $user,                  # omnic
-  $worker_processes,      # number of CPU
-  $error_log,             # /var/log/nginx/error.log warn
-  $pid,                   # /var/run/nginx.pid
-  $worker_connection,     # 1024
-  $default_type,          # application/octet-stream
-  $access_log,            # /var/log/nginx/access.log  main
-  $client_max_body_size,  # 128M
-  $sendfile,              # on
-  $autoindex,             # off
-  $tcp_nopush,            # on
-  $keepalive_timeout,     # 60
-  $gzip,                  # on
-  $gzip_disable,          # "msie6"
-  $gzip_vary,             # on
-  $gzip_proxied,          # any
-  $gzip_comp_level,       # 5
-  $gzip_buffers,          # 16 8k
-  $gzip_http_version,     # 1.1
-  $gzip_types,            # application/vnd.ms-fontobject application/x-font-ttf font/opentype text/plain text/css application/javascript application/x-javascript text/xml application/xml application/xml+rss text/javascript
-  $send_timeout,          # 3600
-  $proxy_read_timeout,    # 3600
+  $user                   = 'nginx',
+  $worker_processes       = 2,
+  $error_log              = '/var/log/nginx/error.log warn',
+  $pid                    = '/var/run/nginx.pid',
+  $worker_connection      = 1024,
+  $default_type           = 'application/octet-stream',
+  $access_log             = '/var/log/nginx/access.log  main',
+  $client_max_body_size   = '128M',
+  $sendfile               = 'on',
+  $autoindex              = 'off',
+  $tcp_nopush             = 'on',
+  $keepalive_timeout      = 10,
+  $gzip                   = undef,
+  $gzip_disable           = undef,
+  $gzip_vary              = undef,
+  $gzip_proxied           = undef,
+  $gzip_comp_level        = undef,
+  $gzip_buffers           = undef,
+  $gzip_http_version      = undef,
+  $gzip_types             = undef,
+  $send_timeout           = undef,
+  $proxy_read_timeout     = undef,
 
   $template='nginx/nginx.conf.erb',
 ){
