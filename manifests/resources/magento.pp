7d1aedd64d8b2c452edf6ecf27661f905f0f6ab9
s1rc0 <sergey.postument@gmail.com>
2014-12-11 18:32:52 +0200

    fix server_name

--- manifests/resources/magento.pp
+++ manifests/resources/magento.pp
@@ -1,14 +1,14 @@
 define nginx::resources::magento(
   $docroot                    = '/var/www/html/',
   $port                       = 80,
-  $server_name,
+  $server_name                = $name,
   $auth_basic_user_file       = '/etc/nginx/.htpasswd',
   $fastcgi_pass               = '127.0.0.1:9000',
   $template                   = 'nginx/magento.erb',
 ){
   include nginx
 
-  file {"/etc/nginx/conf.d/${name}":
+  file {"/etc/nginx/conf.d/${name}.conf":
     content => template($template),
     owner => 'root',
     group => 'root',
