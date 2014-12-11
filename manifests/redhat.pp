0ffdbd8a4f0410797d6f1abaca0b2bcfab98df45
s1rc0 <sergey.postument@gmail.com>
2014-12-11 12:03:11 +0200

    testing

--- manifests/redhat.pp
+++ manifests/redhat.pp
@@ -1,19 +1,4 @@
-# Class: nginx::package::redhat
-#
-# This module manages NGINX package installation on RedHat based systems
-#
-# Parameters:
-#
-# There are no default parameters for this class.
-#
-# Actions:
-#
-# Requires:
-#
-# Sample Usage:
-#
-# This class file is not called directly
-class nginx::package::redhat (
+class nginx::redhat (
   $manage_repo    = true,
   $package_ensure = 'present',
   $package_name   = 'nginx',
@@ -28,11 +13,6 @@ class nginx::package::redhat (
 
   case $::operatingsystem {
     'fedora': {
-      # nginx.org does not supply RPMs for fedora
-      # fedora 18 provides 1.2.x packages
-      # fedora 19 has 1.4.x packages are in
-
-      # fedora 18 users will need to supply their own nginx 1.4 rpms and/or repo
       if $::lsbmajdistrelease and $::lsbmajdistrelease < 19 {
         notice("${::operatingsystem} ${::lsbmajdistrelease} does not supply nginx >= 1.4 packages")
       }
@@ -43,15 +23,9 @@ class nginx::package::redhat (
           $os_rel = $major_dist_release
         }
         default: {
-          # Amazon uses the year as the $::lsbmajdistrelease
           $os_rel = 6
         }
       }
-
-      # as of 2013-07-28
-      # http://nginx.org/packages/centos appears to be identical to
-      # http://nginx.org/packages/rhel
-      # no other dedicated dirs exist for platforms under $::osfamily == redhat
       if $manage_repo {
         yumrepo { 'nginx-release':
           baseurl  => "http://nginx.org/packages/rhel/${os_rel}/\$basearch/",
---
+++ manifests/vhost.pp
@@ -0,0 +1,20 @@
+define nginx::vhost(
+  $docroot,
+  $port,
+  $ssl=false,
+  $priority,
+  $server_name,
+  $template='nginx/vhost.conf.erb',
+){
+  include nginx
+
+  file {"/etc/nginx/conf.d/${name}":
+    content => template($template),
+    owner => 'root',
+    group => 'root',
+    mode => '0640',
+    require => Class['nginx::install'],
+    notify => Class['nginx::service'],
+  }
+
+}
